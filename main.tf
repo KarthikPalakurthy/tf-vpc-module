e8633d88aeebd025d47b03a2a28e9ba0f824cd31
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-04-28 09:32:19 +0100

    terraform

--- main.tf
+++ main.tf
@@ -2,6 +2,16 @@ resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   tags= merge(
     local.common_tags,
-    { Name = "${var.env}-vpc+${count.index}"}
+    { Name = "${var.env}-vpc-{index+1}"}
+    )
+}
+
+resource "aws_subnet" "default" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.cidr_block
+
+  tags = merge(
+    local.common_tags,
+  { Name = "${var.env}-subnet"}
     )
 }
