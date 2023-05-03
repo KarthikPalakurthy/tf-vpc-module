b7619da6a0953d21a23dca3d8e600ef4062edfbd
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-03 11:30:31 +0100

    terraform

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_subnet" "private" {
 
 resource "aws_vpc_peering_connection" "peering" {
   peer_owner_id = data.aws_caller_identity.current.account_id
-  peer_vpc_id = var.default_vpc_id.id
+  peer_vpc_id = var.default_vpc_id
   vpc_id      = aws_vpc.main.id
   tags = merge(
     local.common_tags,
