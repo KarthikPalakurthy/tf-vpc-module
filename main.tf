e8859bbb1752601b45b664ee7cef3970388bb23a
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-03 08:06:48 +0100

    terraform

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "main" {
     )
 }
 
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "peering" {
   peer_owner_id = data.aws_caller_identity.current.account_id
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
@@ -26,4 +26,16 @@ resource "aws_vpc_peering_connection" "foo" {
     local.common_tags,
     { Name = "${var.env}-peering"}
   )
+}
+
+resource "aws_route" "default" {
+  route_table_id            = aws_vpc.main.default_route_table_id
+  destination_cidr_block    = "172.31.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+}
+
+resource "aws_route" "default-vpc" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
