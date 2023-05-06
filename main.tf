5b372c205bdafeb6a6027c0f99b25e037b7dc890
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-06 11:25:37 +0100

    terraform

--- main.tf
+++ main.tf
@@ -3,33 +3,32 @@ resource "aws_vpc" "main" {
   tags= merge(
     local.common_tags,
     { Name = "${var.env}-vpc"}
-    )
+  )
 }
 
-#resource "aws_vpc_peering_connection" "peering" {
-#  peer_owner_id = data.aws_caller_identity.current.account_id
-#  peer_vpc_id = var.default_vpc_id
-#  vpc_id      = aws_vpc.main.id
-#  auto_accept = true
-#  tags = merge(
-#    local.common_tags,
-#    { Name = "${var.env}-peering"}
-#  )
-#}
-#
-#resource "aws_internet_gateway" "igw" {
-#  vpc_id = aws_vpc.main.id
-#  tags= merge(
-#    local.common_tags,
-#    { Name = "${var.env}-igw"}
-#  )
-#}
-#
+resource "aws_vpc_peering_connection" "peering" {
+  peer_owner_id = data.aws_caller_identity.current.account_id
+  peer_vpc_id = var.default_vpc_id
+  vpc_id      = aws_vpc.main.id
+  auto_accept = true
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-peering"}
+  )
+}
 
-#resource "aws_eip" "natgw-ip" {
-#  vpc      = true
-#}
-#
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags= merge(
+    local.common_tags,
+    { Name = "${var.env}-igw"}
+  )
+}
+
+resource "aws_eip" "natgw-ip" {
+  vpc      = true
+}
+
 #resource "aws_nat_gateway" "ngw" {
 #  allocation_id = aws_eip.natgw-ip.id
 #  subnet_id     = aws_subnet.public.*.id[0]
@@ -38,5 +37,12 @@ resource "aws_vpc" "main" {
 #    local.common_tags,
 #    { Name = "${var.env}-natgateway"}
 #  )
+#  //depends_on = [aws_internet_gateway.example]
 #}
-##
+
+
+resource "aws_route" "route" {
+  route_table_id = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+}
